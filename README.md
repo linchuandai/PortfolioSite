236fba0b7eda28ce6f84353dc32625db5c62f96d
Leon Dai <linchuandai@gmail.com>
2020-06-01 22:59:42 -0400

    linked old github.io site

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 This is my porfolio website.
-Built with React.js and hosted on Netlify
-Current Status
+
+Built with React.js and hosted on Netlify.
+
+Current Status: 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/102437ce-68bd-4a6c-bb67-bb9475285a27/deploy-status)](https://app.netlify.com/sites/leondai/deploys)
--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hey look its my super cool website</p>
+        <p>Hey look its my super cool website. Its curerntly under construction.</p>
+        <a className="sitelink" href="https://linchuandai.github.io/site/">
+          Here's my old site if you want to take a look
+        </a>
       </header>
     </div>
   );
